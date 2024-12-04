863db6a61a4aa5b40ffa82f89580783331c9a5ff
Unredix <999daved@gmail.com>
2024-12-04 23:16:09 +0100

    A thing added

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello, Express.js Server!</h1>");
 });
 
+app.get("/about", (req, res) => {
+  res.send("<h1>About page</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>This is where you can contact me :))</h1>");
+});
+app.get("/console", (req, res) => {
+  res.send("<h1>Future console</h1>");
+});
+
 const port = 3000;
 
 app.listen(port, () => {
